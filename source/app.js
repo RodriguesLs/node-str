253f53107485546cb543179b9811b7b070952062
Rodrigues <fr.costa95@gmail.com>
2019-10-26 23:24:14 -0300

    feat(routes): separeted routes, organizing app folders, created a scaffold.

--- source/app.js
+++ source/app.js
@@ -6,16 +6,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//Carregar rotas
+const index = require('./routes/index');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 
-
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: 'Node Store API',
-    version: '0.0.1'
-  });
-});
 
 const create = router.post('/', (req, res, next) => {
   res.status(201).send('success created!');
@@ -32,12 +28,12 @@ const update = router.put('/:id', (req, res, next) => {
 
 const del = router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-  req.status(204).send('Deleted success');
+  res.status(204).send('Deleted success');
 });
 
 // const edit = router.patch('/', (re))
 
-app.use('/', route);
+app.use('/', index);
 app.use('/products', create);
 app.use('/products', update);
 app.use('/products', del);
