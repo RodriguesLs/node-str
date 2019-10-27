4fd25e5f7612642f935f50777eed6b24125b7110
Rodrigues <fr.costa95@gmail.com>
2019-10-26 23:38:08 -0300

    feat(controllers): included controller - implemented architecture MVC

--- source/app.js
+++ source/app.js
@@ -7,35 +7,14 @@ const app = express();
 const router = express.Router();
 
 //Carregar rotas
-const index = require('./routes/index');
+const indexRoute = require('./routes/index');
+const productRoute = require('./routes/product');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 
 
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send('success created!');
-});
-
-const update = router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({ 
-    id: id,
-    item: req.body,
-    message: 'Updated success!'
-  });
-});
-
-const del = router.delete('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(204).send('Deleted success');
-});
-
-// const edit = router.patch('/', (re))
-
-app.use('/', index);
-app.use('/products', create);
-app.use('/products', update);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
