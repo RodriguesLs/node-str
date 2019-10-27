b42364adf7437ed921fb9c4bb84681c278b479ea
Rodrigues <fr.costa95@gmail.com>
2019-10-26 23:14:57 -0300

    feat(routes): initialize REST CRUD
        Included some methods: create (201), update (200) and delete (204).

--- source/app.js
+++ source/app.js
@@ -1,7 +1,14 @@
+'use strict'
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); 
+
 
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
@@ -10,6 +17,29 @@ const route = router.get('/', (req, res, next) => {
   });
 });
 
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send('success created!');
+});
+
+const update = router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).send({ 
+    id: id,
+    item: req.body,
+    message: 'Updated success!'
+  });
+});
+
+const del = router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  req.status(204).send('Deleted success');
+});
+
+// const edit = router.patch('/', (re))
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', update);
+app.use('/products', del);
 
 module.exports = app;
