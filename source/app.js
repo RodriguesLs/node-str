419b2ef5d80a94ecbdc9d83d9e3acbaabe4873fb
Rodrigues <fr.costa95@gmail.com>
2019-10-28 20:35:11 -0300

    feat(mongo, models): included model and connection with mongo
        Implemented the M of MVC, Product.
    
        Implemented connection string with mongoAtlas - mlab, a mongo online server

--- source/app.js
+++ source/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conection with database
+mongoose.connect('mongodb+srv://lies:lies@cluster0-my2zo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 //Carregar rotas
 const indexRoute = require('./routes/index');
